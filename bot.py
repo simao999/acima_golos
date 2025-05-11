5e422681eb3ab674498ef2078520690e4440b39b
simao999 <alfinete-tocaio28@icloud.com>
2025-05-11 21:16:57 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -45,3 +45,30 @@ def send_signals():
 scheduler = BlockingScheduler()
 scheduler.add_job(send_signals, 'interval', minutes=5)
 scheduler.start()
+
+import os
+from telegram import Bot
+import asyncio
+
+bot = Bot("7359570901:AAHV2m-0w21RDHy15VQU8C7CmtwWLoHC_Q4")
+
+async def main():
+    await bot.send_message(chat_id="1487784686", text="Bot está online!")
+
+# Para evitar erro de "port scan", vamos forçar a escuta de uma porta fictícia
+if __name__ == "__main__":
+    import socket
+    from threading import Thread
+
+    def keep_alive():
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('0.0.0.0', 5000))  # Porta fictícia
+        server_socket.listen(1)
+        server_socket.accept()
+
+    # Inicia a "escuta" em segundo plano para evitar erro de porta
+    Thread(target=keep_alive).start()
+
+    # Função principal do bot
+    asyncio.run(main())
+
